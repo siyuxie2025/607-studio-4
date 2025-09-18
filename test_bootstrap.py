0af335488a26f94f5cce7162704996cdb8b6d24e
siyuxie2025 <siyuxie@umich.edu>
2025-09-18 12:16:02 -0400

    Add files via upload

---
+++ test_bootstrap.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+from bootstrap import bootstrap_sample, bootstrap_ci, r_squared
+
+def test_bootstrap_integration():
+    """Test that bootstrap_sample and bootstrap_ci work together"""
+    # This test should initially fail
+    pass
+
+# TODO: Add your unit tests here
+
